ca6efe66318163a67a07dbb6cfdb686f260f7be8
niklaus-geisser-coderoad-com <niklaus.geisser@mojix.com>
2020-07-08 00:24:32 +0200

    Add distance between two markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'webxr';
+export class AppComponent implements OnInit {
+    title = 'webxr';
+    markerVisible = {m0: false, m1: false};
+
+    ngOnInit(): void {
+        console.log('App component');
+    }
+
+
 }
